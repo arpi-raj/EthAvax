cfe939a58a57f85529f386f07f87b1c0b6f9ca50
Arpit Raj <112179932+arpi-raj@users.noreply.github.com>
2023-07-14 23:08:38 +0530

    Update Module_1Project.sol

--- Module_1Project.sol
+++ Module_1Project.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 
 contract ErrorHandlingContract {
     function checkValue(uint256 value) external pure returns (string memory) {
-        require(value > 0, " REQUIRE Value must be greater than 0");
+        require(value > 10, " REQUIRE Value must be greater than 0");
         
         if (value == 42) {
             revert("REVERT Value cannot be 42");
         }
 
-        assert(value < 100);
+        assert(value != 100);
 
         return "Value passes all checks";
     }
